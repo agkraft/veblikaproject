9f20a4e414560fcfb29d9b876bb9719893d8e810
agkraft <agkraft01@gmail.com>
2026-05-29 13:04:31 +0530

    server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://veblikaproject.vercel.app/",
+      "https://veblikaproject.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
